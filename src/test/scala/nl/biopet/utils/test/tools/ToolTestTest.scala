9a5cd843215e2be906a85a243ca4522f77659fc3
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-31 13:54:01 +0100

    finished tests

--- src/test/scala/nl/biopet/utils/test/tools/ToolTestTest.scala
+++ src/test/scala/nl/biopet/utils/test/tools/ToolTestTest.scala
@@ -10,7 +10,8 @@ import org.testng.annotations.Test
 class ToolTestTest extends BiopetTest {
 
   case class Args(num: Int = 0,
-                  inputFile: File = null)
+                  inputFile: File = null,
+                  someBoolean: Boolean = false)
 
   trait TestTool extends ToolCommand[Args] {
     def main(args: Array[String]): Unit = ???
@@ -23,7 +24,7 @@ class ToolTestTest extends BiopetTest {
     class NoDescriptionOption extends ToolTest[Args] {
       def toolCommand: ToolCommand[Args] = new TestTool {
         def argsParser: AbstractOptParser[Args] = new AbstractOptParser[Args]("test") {
-          opt[Int]('n', "num") action { (x,c) => c.copy(num = x) }
+          opt[Int]('n', "num") action { (x, c) => c.copy(num = x) }
         }
       }
     }
@@ -39,7 +40,7 @@ class ToolTestTest extends BiopetTest {
     class ShortDescriptionOption() extends ToolTest[Args] {
       def toolCommand: ToolCommand[Args] = new TestTool {
         def argsParser: AbstractOptParser[Args] = new AbstractOptParser[Args]("test") {
-          opt[Int]('n', "num") text "Bla bla" action { (x,c) => c.copy(num = x) }
+          opt[Int]('n', "num") text "Bla bla" action { (x, c) => c.copy(num = x) }
         }
       }
     }
@@ -49,4 +50,28 @@ class ToolTestTest extends BiopetTest {
       shortDescriptionTest.testArgs()
     }.getMessage shouldBe "7 was not greater than 10"
   }
+
+  @Test
+  def validOption(): Unit = {
+    class ValidOption() extends ToolTest[Args] {
+      def toolCommand: ToolCommand[Args] = new TestTool {
+        def argsParser: AbstractOptParser[Args] = new AbstractOptParser[Args]("test") {
+          opt[Int]('n', "num") text "This is a numeric value" action { (x, c) => c.copy(num = x) }
+          opt[File]('I', "inputFile") required() unbounded() maxOccurs 1 valueName "<file>" action {
+            (x, c) =>
+              c.copy(inputFile = x.getAbsoluteFile)
+          } validate { x =>
+            if (x.exists) success else failure("Input required")
+          } text "Input file (required)"
+          opt[Unit]("someBoolean") unbounded() action { (_, c) =>
+            c.copy(someBoolean = true)
+          }
+        }
+      }
+
+      val ValidOptionTest = new ValidOption()
+      ValidOptionTest.testArgs()
+    }
+  }
 }
+
