42bce316ac5ad5d3ff55a086c20917397d809d18
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-01 16:22:47 +0100

    add more informative description

--- src/main/scala/nl/biopet/utils/test/tools/ToolTest.scala
+++ src/main/scala/nl/biopet/utils/test/tools/ToolTest.scala
@@ -11,8 +11,10 @@ trait ToolTest[T] extends BiopetTest {
   def testArgs(): Unit = {
 
     for (option <- toolCommand.argsParser.optionsForRender){
-      option.desc should not be empty
-      option.desc.length() should be > 10
+      withClue(s"'${option.fullName}' description: ") {
+        option.desc should not be empty
+        option.desc.length() should be > 10
+      }
     }
   }
 }
