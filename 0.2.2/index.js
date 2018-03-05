5d4c4285a6d39540e934e063627c4ffc7a1e3527
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-03-05 15:43:27 +0100

    updated site

---
+++ 0.2.2/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"nl.biopet" : [], "nl" : [], "nl.biopet.utils.test.tools" : [{"trait" : "nl\/biopet\/utils\/test\/tools\/ToolTest.html", "name" : "nl.biopet.utils.test.tools.ToolTest"}], "nl.biopet.utils" : [], "nl.biopet.utils.test" : []};
